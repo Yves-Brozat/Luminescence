97103f720067103ddffbf353358e7ebdd84842c1
Yves-Brozat <yves.brozat@gmail.com>
2019-01-13 19:35:25 +0100

    Add files via upload
    Protocole de communication série avec Chatainge qui sert d'hub OSC avec touchOSC et Processing

---
+++ Luminescence_v2.ino
@@ -0,0 +1,49 @@
+//#define DEBUG
+//#define DEBUGSF
+//#define DEBUGSERIAL
+//#define DEBUGSERVO
+
+//Pins definition
+const byte pinPot[] = {A0,A1};
+const byte pinSns[] = {A2,A7};
+const byte pinSrv = 0;
+
+//mode pour le déplacement du servo
+#define LIGHTREACTIVE 0
+#define REMOTE 1
+int modeServo = LIGHTREACTIVE; // 0 si réactif à la lumiere, 1 si controlé via Serial
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(5);
+  setupServo();
+  setupSF();
+}
+
+void loop() {
+   updateServo();
+   moveServo();
+   
+   sendSerial();
+   receiveSerial();
+   
+   loopSF();
+
+   #ifdef DEBUG
+    Serial.print("Pos : ");
+    Serial.print(pos_servo);
+    Serial.print("    Cible : ");
+    Serial.print(targetPos);
+    Serial.print("    msg_lum : ");
+    Serial.print(msg_lum);
+    Serial.print("    CG: ");
+    Serial.print(analogRead(pinSns[0]));
+    Serial.print("    CD: ");
+    Serial.print(analogRead(pinSns[1]));
+    Serial.print("    CD-CG: ");
+    Serial.print(angleLumiere);
+    Serial.print("    CD+CG: ");
+    Serial.print(luminosite);
+    Serial.println("");
+  #endif
+}
